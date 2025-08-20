b21008904b3763e278fff1eb497b588175337aa6
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-08-20 11:00:38 +0330

    creared Login/page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
 import React from 'react'
 import HomePage from './page/HomePage/HomePage'
+import Login from './page/Login/Login'
 
 const App:React.FC = ()=>{
   return (
     <Router>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
+        <Route path='/Login' element={<Login/>}/>
       </Routes>
     </Router>
   )
