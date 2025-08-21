ebfc629ebcf0ccbafbf03b7bd49d0728979f00fc
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-08-21 10:38:52 +0330

    created register-page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
 import React from 'react'
 import HomePage from '@page/HomePage/HomePage'
 import Login from '@page/Login/Login'
+import Register from '@page/Register/Register'
 
 const App:React.FC = ()=>{
   return (
@@ -9,6 +10,7 @@ const App:React.FC = ()=>{
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/Login' element={<Login/>}/>
+        <Route path='/Register' element={<Register/>}/>
       </Routes>
     </Router>
   )
---
+++ src/page/Register/Register.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register:React.FC = ()=>{
+  return (
+    <>
+    this is Register page
+    </>
+  )
+}
+
+export default Registergit
