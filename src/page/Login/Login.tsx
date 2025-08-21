7b3932c3b6d92ecb7d437d3b0f10173720cf9331
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-08-21 11:08:12 +0330

    fixed page login

--- src/page/Login/Login.tsx
+++ src/page/Login/Login.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Signin from '@components/template/LoginRegister/Signin/Signin'
+import NavbarHome from '@components/template/homePage/NavbarHome/NavbarHome'
 
 const Login:React.FC = ()=>{
   return (
     <>
+    <NavbarHome/>
     <Signin/>
     </>
   )
